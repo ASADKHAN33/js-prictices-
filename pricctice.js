dfb4a57da32dc4951f15522cc0b26a10d3ffa954
ASAD KHAN <99748776+ASADKHAN33@users.noreply.github.com>
2024-06-09 20:16:06 -0700

    Add files via upload

---
+++ pricctice.js
@@ -0,0 +1,66 @@
+// // // // // // // // // // let a = 2;
+// // // // // // // // // // let b = 3;
+// // // // // // // // // // let v =787
+// // // // // // // // // // console.log("a + b + v = ", a + b + v)
+// // // // // // // // // // console.log("a *  b =", a * b )
+// // // // // // // // // // console.log("a / b * v =",a / b * v )
+// // // // // // // // // // console.log("a - b + v = ",a - b + v)
+// // // // // // // // // // console.log("a % v =", a % v)
+// // // // // // // // // // console.log("a ** b =", a ** b )
+// // // // // // // // // let age = +prompt("enter your ")
+// // // // // // // // // let result = age >= 18 ? " you can go":"you can not go";
+// // // // // // // // // console.log(result);
+// // // // // // // // for (  let count=50; count <=5907; count++ );{
+// // // // // // // //     console.log("laylo acha hay bhai");
+// // // // // // // // }
+// // // // // // // //     colsole.log("loob has ended")
+   
+// // // // // // // let sum = 0;
+// // // // // // // for ( var i=1;i<=5;i++)
+// // // // // // //     sum = i;
+// // // // // // // {
+// // // // // // //     console.log("sum =", i)
+// // // // // // // } console.log("i = ",i)
+// // // // // // // console.log("kuch nhi huwa hay bhai")
+
+// // // // // // let i = 1;
+// // // // // // while ( i<=4); {
+// // // // // //  console.log("i ke value hay =");
+// // // // // // i++;
+// // // // // //  }
+ 
+// // // // // let str = "my name is asad khan";
+// // // // // let val = str;
+// // // // // for(let val of str);{
+// // // // //     console.log("val = ", val);
+// // // // // }
+
+// // // // let gamenum = 25;
+// // // // let usernumb = +prompt("Enter a number"); // Convert the input to a number
+
+// // // // while (gamenum !== usernumb) {
+// // // //   usernumb = +prompt("You entered the wrong number, guess again"); // Convert the input to a number
+// // // // }
+// // // // alert("Congratulations, you entered the correct number");
+
+// // // //template letral
+// // // let obj = {
+// // //   item:"pen",
+// // //   price: 10, 
+// // // };
+// // // let outut = `the cost of ${obj.item} is ${obj.price} rs`;
+// // // console.log(outut);
+ 
+// // // let obj = {
+// //   items:"pen",
+// //   price:778,
+// // }
+// // let output = `the price of ${obj.items} is ${obj.price} rs`;
+// // console.log(output);
+ 
+
+//username generator
+// let usernam= prompt("enter your name without spaces");
+// let username = "@" + usernam + usernam.length;{
+//   alert(username)
+// } 
